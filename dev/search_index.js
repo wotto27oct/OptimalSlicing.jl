9d72846c4aa45bd3f3cee7573ca835d57741abff
Documenter.jl <documenter@juliadocs.github.io>
2024-04-09 05:09:57 +0000

    build based on 8a12754

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OptimalSlicing","category":"page"},{"location":"#OptimalSlicing","page":"Home","title":"OptimalSlicing","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OptimalSlicing.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OptimalSlicing]","category":"page"}]
+}
